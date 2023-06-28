ea6e1c6801e5cd9deb47022f7b969d1018f92acc
cirmialexis <1320538+cirmialexis@users.noreply.github.com>
2023-06-28 16:08:36 -0300

    Animations were improved.

--- assets/js/tienda-filter.js
+++ assets/js/tienda-filter.js
@@ -1,6 +1,7 @@
 // Event listeners to make the appropiate elements to appear with their respetive animations, when clicking on a filter button
 
 document.querySelector("#todo").addEventListener("click", () => {
+  const scrollPosition = window.scrollY; // Get the current scroll position
   document.querySelectorAll(".infusiones").forEach(item => item.style.display = 'none');
   document.querySelectorAll(".snacks").forEach(item => item.style.display = 'none');
   document.querySelectorAll(".pasteleria").forEach(item => item.style.display = 'none');
@@ -12,32 +13,39 @@ document.querySelector("#todo").addEventListener("click", () => {
   }, 1);
   setTimeout(() => {
     document.querySelectorAll(".pasteleria").forEach(item => item.style.display = 'block');
+    window.scroll({ top: scrollPosition, behavior: "instant" }); // Restore the scroll position without scrolling animation
   }, 1);
 });
 
 document.querySelector("#infusiones").addEventListener("click", () => {
+  const scrollPosition = window.scrollY; // Get the current scroll position
   document.querySelectorAll(".infusiones").forEach(item => item.style.display = 'none');
   setTimeout(() => {
     document.querySelectorAll(".infusiones").forEach(item => item.style.display = 'block');
+    window.scroll({ top: scrollPosition, behavior: "instant" }); // Restore the scroll position without scrolling animation
   }, 1);
   document.querySelectorAll(".snacks").forEach(item => item.style.display = 'none');
   document.querySelectorAll(".pasteleria").forEach(item => item.style.display = 'none');
 });
 
 document.querySelector("#snacks").addEventListener("click", () => {
+  const scrollPosition = window.scrollY; // Get the current scroll position
   document.querySelectorAll(".infusiones").forEach(item => item.style.display = 'none');
   document.querySelectorAll(".snacks").forEach(item => item.style.display = 'none');
   setTimeout(() => {
     document.querySelectorAll(".snacks").forEach(item => item.style.display = 'block');
+    window.scroll({ top: scrollPosition, behavior: "instant" }); // Restore the scroll position without scrolling animation
   }, 1);
   document.querySelectorAll(".pasteleria").forEach(item => item.style.display = 'none');
 });
 
 document.querySelector("#pasteleria").addEventListener("click", () => {
+  const scrollPosition = window.scrollY; // Get the current scroll position
   document.querySelectorAll(".infusiones").forEach(item => item.style.display = 'none');
   document.querySelectorAll(".snacks").forEach(item => item.style.display = 'none');
   document.querySelectorAll(".pasteleria").forEach(item => item.style.display = 'none');
   setTimeout(() => {
     document.querySelectorAll(".pasteleria").forEach(item => item.style.display = 'block');
+    window.scroll({ top: scrollPosition, behavior: "instant" }); // Restore the scroll position without scrolling animation
   }, 1);
 });
